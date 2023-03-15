28531816c252bdd330f4f3abce75c9c0b65e14a5
Bhargavi Nagaraj Naik <bhargavinagarajnaik@clarivate.com>
2023-03-15 21:08:35 +0530

    Create uset page

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,46 @@
+package com.example.smack
+
+import android.content.res.Resources
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smack.databinding.ActivityCreateUserBinding
+import kotlin.random.Random
+
+class CreateUserActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCreateUserBinding
+    var usedImage = "profiledefault"
+    var usedColor = "[0.5, 0.5, 0.5, 1]"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createUserAvatarImage.setOnClickListener {
+            var color = Random.nextInt(2)
+            var avatar = Random.nextInt(28)
+
+            usedImage = if (color == 0) {
+                "light${avatar}"
+            } else {
+                "dark${avatar}"
+            }
+
+            println(avatar)
+            var resourceId = resources.getIdentifier(usedImage, "drawable", packageName)
+
+            binding.createUserAvatarImage.setImageResource(resourceId)
+        }
+
+        binding.generateBackgroundBtn.setOnClickListener {
+            var r = Random.nextInt(255)
+            var g = Random.nextInt(255)
+            var b = Random.nextInt(255)
+
+            binding.createUserAvatarImage.setBackgroundColor(Color.rgb(r, g, b))
+
+            usedColor = "[${r.toDouble()/255},${g.toDouble()/255}, ${b.toDouble()/255},1]"
+            println(r)
+        }
+    }
+}
--- app/src/main/java/com/example/smack/LoginActivity.kt
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smack.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpBtn.setOnClickListener {
+            var intent = Intent(this, CreateUserActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
